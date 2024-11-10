731358e626216167af607b57b0dd4c2169875f37
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-10 09:39:32 +0600

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+
+const uri = `mongodb+srv://${user}:${pass}@cluster0.zee3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) =>{
     res.send('SERVER IS RUNNING......')
 });
 
 app.listen(port, () => {
     console.log(`server is running from ${port}`);
-})
+});
